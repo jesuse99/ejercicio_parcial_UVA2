0a48765993fd4248691c5b09ff4b1b5176921823
jesuse99 <jespinoza@uade.edu.ar>
2025-06-05 14:32:44 -0300

    update page mode with jquery

--- mode.js
+++ mode.js
@@ -1,118 +1,113 @@
+$('#mode_light').click(setModeLight);
+$('#mode_dark').click(setModeDark);
+getMode();
 
-// const modo = document.getElementById("modo");
-// modo.addEventListener('click', function () {
+function setModeLight() {
+    $('#mode_icon').removeClass('bi-moon-fill').addClass('bi-sun-fill');
 
-//     alert("aaaaa");
+    $('body').removeClass('bg-black').addClass('bg-white');
+    $('nav').removeClass('bg-dark').addClass('bg-danger');
 
-
-//     if (modo.innerText == 0) {
-//         modo.value = 1;
-//     } else {
-//         modo.innerText = 0;
-//     }
-// });
-
-function setModo() {
-
-    const modo = document.getElementById("modo");
-    if (modo.innerText == 0) {
-
-        const body = document.querySelector("body");
-        body.classList.replace("bg-black", "bg-white");
-
-        const nav = document.querySelector("nav");
-        nav.classList.replace("bg-dark", "bg-danger");
-
-        const title = document.getElementById("title");
-        if (title) {
-            title.classList.remove("bg-primary");
-            title.style.background = "#2f4f4f";
-        }
-
-        const table = document.querySelector("table");
-        if (table) {
-            table.classList.replace("table-dark", "table-light");
-        }
-
-        const sections = document.querySelectorAll("#section");
-        if (sections) {
-            sections.forEach(element => {
-                element.classList.replace("text-white", "text-black");
-                element.classList.replace("bg-dark", "bg-body-tertiary");
-            })
-        }
-
-        const lists = document.querySelectorAll("#list");
-        if (lists) {
-            lists.forEach(element => {
-                element.setAttribute("data-bs-theme", "light");
-            })
-        }
-
-        const card = document.getElementById("card");
-        if (card) {
-            card.setAttribute("data-bs-theme", "light");
-        }
-
-        const form = document.getElementById("formulario");
-        if (form) {
-            form.setAttribute("data-bs-theme", "light");
-            form.classList.replace("bg-dark", "bg-light");
-            form.classList.add("text-black");
-        }
-
-        const footer = document.querySelector("footer");
-        footer.classList.replace("bg-dark", "bg-danger");
-        modo.innerText = 1;
-    } else {
-
-        const body = document.querySelector("body");
-        body.classList.replace("bg-white", "bg-black");
-
-        const nav = document.querySelector("nav");
-        nav.classList.replace("bg-danger", "bg-dark");
-
-        const title = document.getElementById("title");
-        if (title) {
-            title.style.background = "#0D6EFD";
-        }
-
-        const table = document.querySelector("table");
-        if (table) {
-            table.classList.replace("table-light", "table-dark");
-        }
-
-        const sections = document.querySelectorAll("#section");
-        if (sections) {
-            sections.forEach(element => {
-                element.classList.replace("text-black", "text-white");
-                element.classList.replace("bg-body-tertiary", "bg-dark");
-            })
-        }
-
-        const lists = document.querySelectorAll("#list");
-        if (lists) {
-            lists.forEach(element => {
-                element.setAttribute("data-bs-theme", "dark");
-            })
-        }
-
-        const card = document.getElementById("card");
-        if (card) {
-            card.setAttribute("data-bs-theme", "dark");
-        }
-
-
-        const form = document.getElementById("formulario");
-        if (form) {
-            form.setAttribute("data-bs-theme", "dark");
-            form.classList.replace("bg-light", "bg-dark");
-            form.classList.remove("text-black");
-        }
-
-        const footer = document.querySelector("footer");
-        footer.classList.replace("bg-danger", "bg-dark");
-        modo.innerText = 0;
+    const title = $('#title');
+    if (title) {
+        title.removeClass('bg-primary');
+        title.css('background', '#2f4f4f');
     }
+
+    const table = $('table');
+    if (table) {
+        table.removeClass('table-dark');
+        table.addClass('table-light');
+    }
+
+    const sections = $('.section');
+    if (sections) {
+        sections.each(function () {
+            $(this).removeClass('text-white').addClass('text-black');
+            $(this).removeClass('bg-dark').addClass('bg-body-tertiary');
+        }).get();
+    }
+
+    const lists = $('.list');
+    if (lists) {
+        lists.each(function () {
+            $(this).attr('data-bs-theme', 'light');
+        }).get()
+    }
+
+    const card = $('#card');
+    if (card) {
+        card.attr('data-bs-theme', 'light');
+    }
+
+    const form = $('#formulario');
+    if (form) {
+        form.attr('data-bs-theme', 'light');
+        form.removeClass('bg-dark');
+        form.addClass('bg-light').addClass('text-black');
+    }
+
+    $('footer').removeClass('bg-dark').addClass('bg-danger');
+
+    localStorage.setItem("pageMode", 0);
 };
+
+function setModeDark() {
+    $('#mode_icon').removeClass('bi-sun-fill').addClass('bi-moon-fill');
+
+    $('body').removeClass('bg-white').addClass('bg-black');
+    $('nav').removeClass('bg-danger').addClass('bg-dark');
+
+    const title = $('#title');
+    if (title) {
+        title.removeClass('bg-primary');
+        title.css('background', '#0D6EFD');
+    }
+
+    const table = $('table');
+    if (table) {
+        table.removeClass('table-light');
+        table.addClass('table-dark');
+    }
+
+    const sections = $('.section');
+    if (sections) {
+        sections.each(function () {
+            $(this).removeClass('text-black').addClass('text-white');
+            $(this).removeClass('bg-body-tertiary').addClass('bg-dark');
+        }).get();
+    }
+
+    const lists = $('.list');
+    if (lists) {
+        lists.each(function () {
+            $(this).attr('data-bs-theme', 'dark');
+        }).get()
+    }
+
+    const card = $('#card');
+    if (card) {
+        card.attr('data-bs-theme', 'dark');
+    }
+
+    const form = $('#formulario');
+    if (form) {
+        form.attr('data-bs-theme', 'dark');
+        form.removeClass('bg-light').removeClass('text-black');
+        form.addClass('bg-dark');
+    }
+
+    $('footer').removeClass('bg-danger').addClass('bg-dark');
+
+    localStorage.setItem("pageMode", 1);
+};
+
+function getMode() {
+    const pageMode = localStorage.getItem("pageMode");
+    if (pageMode && pageMode == 0) {
+        setModeLight();
+    } else {
+        setModeDark();
+    }
+}
 
